07770a9ae72b69f1c57b600c8544b038d9524bbc
juan socorro <46764865+juansm17@users.noreply.github.com>
2021-04-08 23:53:09 -0400

    Update Share.js

--- src/Screen/Share.js
+++ src/Screen/Share.js
@@ -1,15 +1,14 @@
-import React, { Component } from 'react';
-import { Share, View, Button } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import React from 'react';
+import { Share, View, Button, Text, Alert } from 'react-native';
 
-class Shares extends Component {
-    onShare = async () => {
+const InviteFriend = () => {
+    const onShare = async () => {
         try {
             const result = await Share.share({
                 message:
-                    'Te invito a probar FakedIn con el siguiente link:https://expo.io/artifacts/d95bfaf3-4261-4192-bd95-ac4f58062e66',
+                    'Hello!. You have been invited to join the LinkedIn clone! ✔,' +
+                    '\nDownload the application at this link: https://expo.io/artifacts/d95bfaf3-4261-4192-bd95-ac4f58062e66 '
             });
-
             if (result.action === Share.sharedAction) {
                 if (result.activityType) {
 
@@ -24,15 +23,33 @@ class Shares extends Component {
         }
     };
 
-    render() {
-        return (
-            <View style={{ marginTop: 70 }}>
-                <ScrollView>
-                    <Button onPress={this.onShare} title="Share" />
-                </ScrollView>
-            </View>
-        );
-    }
-}
+   
 
-export default Shares;
+    return (
+        <View>
+            <Text style={{
+                marginTop: 40,
+                fontSize: 25,
+                textAlign: "center",
+                color: "#000"
+            }}>
+                Obten mas conecciones!!
+      </Text>
+            <Text style={{
+                marginHorizontal: 55,
+                textAlign: "center",
+                marginVertical: 5,
+                opacity: 0.4,
+                color: "#333"
+            }}>
+                Invita a tus amigos para aumentar tus conecciones, y asi mismo. Tus oportunidades de seguir creciendo
+      </Text>
+            <View style={{ marginTop: 40, width: "80%", alignSelf: "center" }}>
+                <Button onPress={onShare} title="Share" />
+            </View>
+
+        </View>
+    );
+};
+
+export default InviteFriend;
