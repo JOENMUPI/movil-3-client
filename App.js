74f800b05e65803fa04ad2fb7d54a38e71fe7a9a
juan socorro <46764865+juansm17@users.noreply.github.com>
2021-04-18 20:03:57 -0400

    Update App.js

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import skills from './src/Screen/skills';
 import education from './src/Screen/education';
 import Share from './src/Screen/Share';
 import Contacts from './src/Screen/Contacts';
+import Company from './src/Screen/Company';
 
 const Stack = createStackNavigator();
 
@@ -16,6 +17,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+              <Stack.Screen name="Company" component={Company} options={{ title: "Company", headerShown: false }}></Stack.Screen>
              <Stack.Screen name=" Contacts" component={Contacts} options={{ title: " Contacts", headerShown: false }}></Stack.Screen>
             <Stack.Screen name="Share" component={Share} options={{ title: "Share", headerShown: false }}></Stack.Screen>
            <Stack.Screen name="education" component={education} options={{ title: "education", headerShown: false }}></Stack.Screen>
